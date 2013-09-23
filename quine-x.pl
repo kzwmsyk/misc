43619dc659eda00ccc93537448507f13f9ef84b8
Masayuki Kozawa <kozawa@nhn.com>
2013-09-24 01:32:00 +0900

    quine (executable version)

---
+++ quine-x.pl
@@ -0,0 +1,9 @@
+#!/opt/local/bin/perl
+use strict;
+use warnings;
+print join "__DATA__\n", (join "", <DATA>) x 2;
+__DATA__
+#!/opt/local/bin/perl
+use strict;
+use warnings;
+print join "__DATA__\n", (join "", <DATA>) x 2;
